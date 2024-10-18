c1177a53de3031033a30ae3a1a75a7d23b39c9ba
Mohan-8 <smohanakrishnan82@gmail.com>
2024-10-18 19:27:37 +0530

    first commit

---
+++ static/chunks/app/layout-713a5275aa8d24d0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1598:function(n,e,u){Promise.resolve().then(u.t.bind(u,2778,23))},2778:function(){}},function(n){n.O(0,[515,971,117,744],function(){return n(n.s=1598)}),_N_E=n.O()}]);
